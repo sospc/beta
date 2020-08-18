8b83bf041c8b5f3b31ec0cf7c5d47ce728719f40
sospc <sospc1@gmail.com>
2020-08-18 23:22:52 +0100

    Update app.js

--- apps/banglerun/app.js
+++ apps/banglerun/app.js
@@ -325,7 +325,7 @@ var lastKeyPress = 0;
 function btnPressed() {
 if (NRF.getSecurityStatus().connected) {
 Bangle.buzz();
-E.showMessage("You did a Replay\nSaving...\n","_");
+E.showMessage("You did a Replay\nSaving...\n","__");
 var time = getTime();
 var timeSince = time - lastKeyPress;
 lastKeyPress = time;
