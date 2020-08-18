9094a0800859758175261b903bd39dad69a7903d
sospc <sospc1@gmail.com>
2020-08-18 22:51:42 +0100

    Update app.js

--- apps/banglerun/app.js
+++ apps/banglerun/app.js
@@ -325,14 +325,14 @@ var pressTimeout;
 var lastKeyPress = 0;
 function btnPressed() {
 if (NRF.getSecurityStatus().connected) {
-Bangle.buzz();
-E.showMessage("You did a Replay\nSaving...\n","");
 var time = getTime();
 var timeSince = time - lastKeyPress;
 lastKeyPress = time;
 if (timeSince < 10) return; // ignore if < 10 sec ago 
 if (pressTimeout) return; // ignore a second press within the 10 sec
 // wait 5 seconds
+Bangle.buzz();
+E.showMessage("You did a Replay\nSaving...\n","");
 pressTimeout = setTimeout(function() {
 pressTimeout = undefined;
 NRF.sendHIDReport([0,0,30,0,0,0,0,0], function() {
