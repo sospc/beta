a7498c794f7f6f228b37c4180de55112fdbbe9a1
sospc <sospc1@gmail.com>
2020-08-18 19:52:47 +0100

    Update app.js

--- apps/imgclock/app.js
+++ apps/imgclock/app.js
@@ -28,47 +28,6 @@ function createBgImg() {
 }
 if (!bgimg || !bgimg.length) createBgImg();
 
-/*
-function draw() {
-  var t = new Date();
-  var hours = t.getHours();
-  var meridian = "";
-  if (is12Hour) {
-    meridian = (hours < 12) ? "AM" : "PM";
-    hours = ((hours + 11) % 12) + 1;
-  }
-  // quickly set background image
-  new Uint8Array(cg.buffer).set(bgimg);
-  // draw time
-  cg.setColor(inf.col);
-  var x = 74 + 32 * inf.align;
-  cg.setFont("7x11Numeric7Seg",3);
-  cg.setFontAlign(1,-1);
-  cg.drawString(hours, x, 0);
-  x+=2;
-  if (t.getSeconds() & 1)
-    cg.fillRect(x, 10, x+2, 10+2).fillRect(x, 20, x+2, 20+2);
-  x+=6;
-  cg.setFontAlign(-1,-1);
-  cg.drawString(("0"+t.getMinutes()).substr(-2), x, 0);
-  x+=44;
-  cg.setFont("7x11Numeric7Seg",1);
-  cg.drawString(("0"+t.getSeconds()).substr(-2), x, 20);
-  cg.setFont("6x8",1);
-  cg.drawString(meridian, x+2, 0);
-  let date = locale.date(t);
-  if (cg.stringWidth(date) < IW-64) {
-    cg.setFontAlign(0, -1);
-    cg.drawString(date,IW/2+32*inf.align,IH-8);
-  } else {
-    cg.setFontAlign(inf.align, -1);
-    cg.drawString(date,IW*(inf.align+1)/2,IH-8);
-  }
-  // draw to screen
-  g.drawImage(cgimg,IX,IY+OY);
-}
-*/
-E.showMessage("","Pitch X");
 // Replays
 var pressTimeout;
 var lastKeyPress = 0;
