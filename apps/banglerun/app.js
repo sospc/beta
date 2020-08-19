3f120e7ceb943282c39b7bb71b83a9a4993d7f71
sospc <sospc1@gmail.com>
2020-08-19 14:39:22 +0100

    Update app.js

--- apps/banglerun/app.js
+++ apps/banglerun/app.js
@@ -303,11 +303,11 @@ function stop() {
   draw();
 }
 
-//Bangle.on('GPS', handleGps);
+Bangle.on('GPS', handleGps);
 Bangle.on('HRM', handleHrm);
 Bangle.on('step', handleStep);
 
-//Bangle.setGPSPower(1);
+Bangle.setGPSPower(1);
 Bangle.setHRMPower(1);
 
 g.clear();
